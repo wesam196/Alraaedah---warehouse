39cb6051420b0334595ea59be1df327f8f516078
wesam <wesam.a.s196@gmail.com>
2025-07-03 00:16:29 +0300

    create pledge table

---
+++ app/Http/Controllers/PledgeController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PledgeController extends Controller
+{
+    //
+}
--- database/migrations/2025_07_02_211252_create_pledges_table.php
+++ database/migrations/2025_07_02_211252_create_pledges_table.php
@@ -11,16 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('pledges', function (Blueprint $table) {
             $table->id();
-            $table->string('productName');
-            $table->unsignedBigInteger("quantity");
-            $table->unsignedBigInteger('category');
+            $table->unsignedBigInteger("product_id");
 
             $table->timestamps();
 
-
-            $table->foreign('category')->references('id')->on('category')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('pledges');
     }
 };
