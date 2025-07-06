6ff38d4705413198e3ad8a6ba6a60f7c33603eb3
wesam <wesam.a.s196@gmail.com>
2025-07-06 05:16:40 +0300

    update nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -6,7 +6,7 @@
       font-weight: bold;
       
     }
-    header {
+    .header {
       background:linear-gradient(45deg, #2f87a2 0, #2f87a2 100%);
       color: white;
       padding: 15px 20px;
@@ -15,7 +15,7 @@
       justify-content: space-between;
     }
 
-    header .logo img {
+    .header  img {
       width: 200px;
       height: auto;
     }
@@ -23,9 +23,30 @@
 
 
 </style>
-<header>
-    <div class="logo">
-        <img src="images/logo.png"  class="img-fluid" alt="الشعار">
-    </div>
 
-  </header>
+
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-light  header">
+  <a class="navbar-brand" href="#">
+     <img src="images/logo.png"  class="img-fluid" alt="الشعار">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="#"> الرئيسية </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="/dashboard">لوحة التحكم</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">المستخدمين</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
